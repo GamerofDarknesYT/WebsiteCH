9efa02bbb3b0e48b09b113656fd8b01922639eff
Rok Karmel <rok.karmel@gmail.com>
2023-01-06 15:48:16 +0100

    Updated file

--- WebsiteCheck3.0.py
+++ WebsiteCheck3.0.py
@@ -3,14 +3,22 @@ import yaml
 from yaml import Loader
 
 r = requests.get("https://monitoring.cloud.1home.io")
+datasend = {"status": "success"}
+
+posturl = "https://monitoring.cloud.1home.io/alert-test"
 
 print(r.ok)
 print(r.status_code)
 
 if r.status_code >= 400 and r.status_code <= 600:
-    print("Webside not working")
+    print("Website not working")
+    r = requests.post(url = posturl, data = datasend)
 
-#yaml_file = open("URL.yaml", "r")
-#data = yaml.load(yaml_file, Loader=Loader)
+print(datasend)
+response = r.text
+print("The response is:%s" %response)
 
-#print(data)
+yaml_file = open("URL.yaml", "r")
+data = yaml.load(yaml_file, Loader=Loader)
+
+print(data)
